c47ff7f3ce6a50fcbc9c87e36e168f7ea4bcdafb
David <davidstepanov@yahoo.com>
2022-10-11 12:39:41 -0400

    commit tree f09c27b33e392e2702fd7dfd56c5e46bfa111ec6

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+
+
+katz_deli = []
+def line array
+    if array == []
+        puts "the line is empty"
+    elsif array.length == 1
+        puts "There is 1 person ahead of you"
+    else 
+        puts "There are #{array.length} people ahead of you"
+    end
+end
+
+
+def take_a_number(array, name)
+spot = array.length + 1
+puts "Welcome, #{name}. You are number #{spot} in line"
+return array.push("#{name}")
+end
+
+def now_serving array
+    puts "Currently serving #{array[0]}"
+    return array.shift()
+end
+
+
+
+line(katz_deli)
+take_a_number(katz_deli, "David")
+
+take_a_number(katz_deli, "John")
+now_serving(katz_deli)
+
+
+
